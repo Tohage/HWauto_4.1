d44a3bd9a500572e4bf7834779702f9ec5144a2b
Tohage89 <avgorin89@gmail.com>
2025-07-01 21:50:05 +0300

    edit tests and build gradle

--- src/test/java/CardDeliveryOrderTests.java
+++ src/test/java/CardDeliveryOrderTests.java
@@ -1,8 +1,4 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,15 +10,9 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class CardDeliveryOrderTests {
 
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
 
     @BeforeEach
     void setUp() {
@@ -36,14 +26,14 @@ public class CardDeliveryOrderTests {
 
     @Test
     void fieldsAreFilledCorrectly() {
-        $$("[placeholder='Город']").find(visible).setValue("Волгоград");
+        $("[placeholder='Город']").setValue("Волгоград");
         //$("[data-test-id='date'] .input__control").click();
-        $$("[data-test-id='date'] .input__control").find(visible).press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $$("[data-test-id='date'] .input__control").find(visible).setValue(dataGenerate(3));
-        $$("[data-test-id='name'] .input__control").find(visible).setValue("Иванов Иван");
-        $$("[data-test-id='phone'] .input__control").find(visible).setValue("+79876543210");
-        $$("[data-test-id='agreement']").find(visible).click();
-        $$(".button__text").find(visible).click();
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(dataGenerate(3));
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79876543210");
+        $("[data-test-id='agreement']").click();
+        $(".button__text").click();
         $("div .notification__content").should(visible, Duration.ofSeconds(15));
         $("div .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dataGenerate(3)));
 
